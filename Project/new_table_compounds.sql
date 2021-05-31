81e1375e5fdcd1ba9f1b980df9d80d3768e95cc4
mmsantaolalla <61518634+mmsantaolalla@users.noreply.github.com>
2021-06-01 00:42:31 +0200

    another file

---
+++ Project/new_table_compounds.sql
@@ -0,0 +1,20 @@
+create table compound_elements{
+	compound_id INT NOT NULL PRIMARY KEY,
+	C int NOT NULL default 0,
+	N int NOT NULL default 0,
+	Cl int NOT NULL default 0,
+	O int NOT NULL default 0,
+	H int NOT NULL default 0,
+	P int NOT NULL default 0,
+	S int NOT NULL default 0,
+
+	FOREIGN KEY(compound_id) REFERENCES compounds(compound_id) ON DELETE CASCADE, 
+	INDEX compound_elements_C_index (C),
+	INDEX compound_elements_N_index (N),
+	INDEX compound_elements_Cl_index (Cl),
+	INDEX compound_elements_O_index (O),
+	INDEX compound_elements_H_index (H),
+	INDEX compound_elements_P_index (P),
+	INDEX compound_elements_S_index (S),
+
+} ENGINE=INNODB;
